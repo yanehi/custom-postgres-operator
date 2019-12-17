783064caff888eab5b713cfa8cccb0986ee277a6
Yannic Nevado Hidalgo <yannic.hidalgo@stud.h-da.de>
2019-12-17 14:14:45 +0100

    change name from acid-minimal-cluster to postgres-instance for statefulsets

--- create_operator_setup.sh
+++ create_operator_setup.sh
@@ -2,22 +2,22 @@
 
 set -x
 
-kubectl create -f manifests/configmap.yaml --namespace zalando-postgres
+kubectl create -f manifests/configmap.yaml -n zalando-postgres
 
-kubectl create -f manifests/operator-service-account-rbac.yaml --namespace zalando-postgres
+kubectl create -f manifests/operator-service-account-rbac.yaml -n zalando-postgres
 
-kubectl create -f manifests/postgres-operator.yaml --namespace zalando-postgres
-
-sleep 10
-
-kubectl get pod -l name=postgres-operator --namespace zalando-postgres
-
-kubectl create -f manifests/minimal-postgres-manifest.yaml --namespace zalando-postgres
+kubectl create -f manifests/postgres-operator.yaml -n zalando-postgres
 
 sleep 10
 
-kubectl get postgresql --namespace zalando-postgres
+kubectl get pod -l name=postgres-operator -n zalando-postgres
 
-kubectl get pods -l application=spilo -L spilo-role --namespace zalando-postgres
+kubectl create -f manifests/postgres-instance.yaml -n zalando-postgres
 
-kubectl get svc -l application=spilo -L spilo-role --namespace zalando-postgres
+sleep 10
+
+kubectl get postgresql -n zalando-postgres
+
+kubectl get pods -l application=spilo -L spilo-role -n zalando-postgres
+
+kubectl get svc -l application=spilo -L spilo-role -n zalando-postgres
--- reset_postgres_operator_setup.sh
+++ reset_postgres_operator_setup.sh
@@ -8,14 +8,14 @@ kubectl delete clusterrolebindings zalando-postgres-operator -n zalando-postgres
 
 kubectl delete serviceaccounts  zalando-postgres-operator -n zalando-postgres
 
-kubectl delete secrets foo-user.acid-minimal-cluster.credentials postgres.acid-minimal-cluster.credentials standby.acid-minimal-cluster.credentials zalando.acid-minimal-cluster.credentials -n zalando-postgres
+kubectl delete secrets foo-user.postgres-instance.credentials postgres.postgres-instance.credentials standby.postgres-instance.credentials zalando.postgres-instance.credentials -n zalando-postgres
 
 kubectl delete configmaps postgres-operator -n zalando-postgres
 
-kubectl delete postgresql acid-minimal-cluster --namespace zalando-postgres
+kubectl delete postgresql postgres-instance -n zalando-postgres
 
-kubectl delete service acid-minimal-cluster acid-minimal-cluster-repl -n zalando-postgres
+kubectl delete service postgres-instance postgres-instance-repl -n zalando-postgres
 
 kubectl delete poddisruptionbudgets postgres-acid-minimal-cluster-pdb -n zalando-postgres
 
-kubectl delete pvc pgdata-acid-minimal-cluster-0 pgdata-acid-minimal-cluster-1 -n zalando-postgres
+# kubectl delete pvc pgdata-acid-minimal-cluster-0 pgdata-acid-minimal-cluster-1 pgdata-acid-minimal-cluster-2 -n zalando-postgres
