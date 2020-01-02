f0003c48a8f6b202a465999e0dc6f005c59306d5
Yannic Nevado Hidalgo <yannic.hidalgo@stud.h-da.de>
2020-01-02 17:36:32 +0100

    enable pod affinity for postgres instances, increase volume size

--- reset_postgres_operator_setup.sh
+++ reset_postgres_operator_setup.sh
@@ -6,7 +6,7 @@ kubectl delete clusterroles zalando-postgres-operator -n zalando-postgres
 
 kubectl delete clusterrolebindings zalando-postgres-operator -n zalando-postgres
 
-kubectl delete serviceaccounts  zalando-postgres-operator -n zalando-postgres
+kubectl delete serviceaccounts zalando-postgres-operator -n zalando-postgres
 
 kubectl delete secrets foo-user.postgres-instance.credentials postgres.postgres-instance.credentials standby.postgres-instance.credentials zalando.postgres-instance.credentials -n zalando-postgres
 
@@ -14,8 +14,12 @@ kubectl delete configmaps postgres-operator -n zalando-postgres
 
 kubectl delete postgresql postgres-instance -n zalando-postgres
 
-kubectl delete service postgres-instance postgres-instance-repl -n zalando-postgres
+sleep 3
 
-kubectl delete poddisruptionbudgets postgres-acid-minimal-cluster-pdb -n zalando-postgres
+kubectl delete service postgres-instance postgres-instance-config postgres-instance-repl -n zalando-postgres
+
+# sleep 3
+
+# kubectl delete poddisruptionbudgets postgres-acid-minimal-cluster-pdb -n zalando-postgres
 
 # kubectl delete pvc pgdata-acid-minimal-cluster-0 pgdata-acid-minimal-cluster-1 pgdata-acid-minimal-cluster-2 -n zalando-postgres
